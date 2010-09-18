6bb8cae87dc31cc70f845d053de109f53c4f21db
Dan Mayer <Danmayer@gmail.com>
2010-09-18 14:43:51 -0400

    fixed accidnetly disabled internet access

--- phonegap-android/assets/www/fitbit-app.js
+++ phonegap-android/assets/www/fitbit-app.js
@@ -2,8 +2,8 @@
 // This file has the app version of the methods
 
 //var url_base = "http://danmayer.dnsalias.com:4567";
-var url_base = "https://fitbit-widget-staging.heroku.com";
-//var url_base = "https://fitbit-widget.heroku.com";
+//var url_base = "https://fitbit-widget-staging.heroku.com";
+var url_base = "https://fitbit-widget.heroku.com";
 var user = "";
 var pass = "";
 
