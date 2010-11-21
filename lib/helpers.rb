e0e8e203fbba3bd9e9bec6665efa836e9614e9af
Dan Mayer <Danmayer@gmail.com>
2010-11-20 23:57:21 -0500

    refactored out caching

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -51,35 +51,53 @@ def get_calories_eaten_data(account, start_date)
   @calories_eaten = @fitbit.get_eaten_calories(start_date)[:calories_xml] rescue nil
 end
 
+def cached_data(key)
+  FileUtils.mkdir('tmp/') unless File.exists?('tmp/')
+  cache_path = "tmp/#{key}.json"
+
+  data = if File.exists?(cache_path)
+           JSON.parse(File.read(cache_path))
+         else
+           result = yield
+           File.open(cache_path, 'w') {|f| f.write(result.to_json) }
+           result
+         end
+end
+
 #TODO man this needs to be cleaned up, caching, repeated code, object creation
 def get_account_data(account, start_date = nil, end_date = nil)
   start_date ||= Chronic.parse('1 day ago')
   end_date ||= Chronic.parse('8 days ago')
 
-  #TODO move to cache method
   begin
-    FileUtils.mkdir('tmp/') unless File.exists?('tmp/')
-    cache_path = "tmp/#{account.fitbit_email}-#{format_date(start_date)}.json"
-    data = if File.exists?(cache_path)
-             JSON.parse(File.read(cache_path))
-           else
-             @fitbit = RubyFitbit.new(account.fitbit_email,account.fitbit_pass)
-             data = @fitbit.get_avg_data(start_date, end_date) 
-             #for faster debugging
-             #data = {'steps' => 0.22, 'calories' => 0.22, 'miles_walked' => 0.22}
-             File.open(cache_path, 'w') {|f| f.write(data.to_json) }
-             data
-           end
+    account_date_key = "#{account.fitbit_email}-#{format_date(start_date)}"
+    data = cached_data(account_date_key) {
+      @fitbit = RubyFitbit.new(account.fitbit_email,account.fitbit_pass)
+      data = @fitbit.get_avg_data(start_date, end_date) 
+      #for faster debugging
+      #data = {'steps' => 0.22, 'calories' => 0.22, 'miles_walked' => 0.22}   
+      data
+    }
+
+#     FileUtils.mkdir('tmp/') unless File.exists?('tmp/')
+#     cache_path = "tmp/#{account.fitbit_email}-#{format_date(start_date)}.json"
+#     data = if File.exists?(cache_path)
+#              JSON.parse(File.read(cache_path))
+#            else
+#              @fitbit = RubyFitbit.new(account.fitbit_email,account.fitbit_pass)
+#              data = @fitbit.get_avg_data(start_date, end_date) 
+#              #for faster debugging
+#              #data = {'steps' => 0.22, 'calories' => 0.22, 'miles_walked' => 0.22}
+#              File.open(cache_path, 'w') {|f| f.write(data.to_json) }
+#              data
+#            end
     
-    cache_path = "tmp/#{account.fitbit_email}-aggregate-#{format_date(start_date)}.json"
-    aggregate_data = if File.exists?(cache_path)
-                       JSON.parse(File.read(cache_path))
-                     else
-                       @fitbit = RubyFitbit.new(account.fitbit_email,account.fitbit_pass) unless @fitbit
-                       aggregate_data = @fitbit.get_aggregated_data(start_date, end_date) 
-                       File.open(cache_path, 'w') {|f| f.write(aggregate_data.to_json) }
-                       aggregate_data
-                     end
+
+    account_aggregate_key = "#{account.fitbit_email}-aggregate-#{format_date(start_date)}"
+    aggregate_data = cached_data(account_aggregate_key) {
+      @fitbit = RubyFitbit.new(account.fitbit_email,account.fitbit_pass) unless @fitbit
+      aggregate_data = @fitbit.get_aggregated_data(start_date, end_date) 
+    }
     
     @avg_steps = '%.2f' % data['steps'].to_f
     @avg_calories = '%.2f' % data['calories'].to_f
